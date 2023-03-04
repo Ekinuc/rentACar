4a297c77a2d8b27b8f8b854a25b4eebe6c02765b
alier <alier@LAPTOP-Q97OKIR9>
2023-03-05 00:43:45 +0300

    Commit 1

--- rentACar/src/main/java/kodlama/io/rentACar/bussines/abstracts/BrandService.java
+++ rentACar/src/main/java/kodlama/io/rentACar/bussines/abstracts/BrandService.java
@@ -2,9 +2,11 @@ package kodlama.io.rentACar.bussines.abstracts;
 
 import java.util.List;
 
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.bussines.requests.CreateBrandRequest;
+import kodlama.io.rentACar.bussines.responses.GetAllBrandsResponse;
 
 public interface BrandService {
-	List<Brand> getAll();
+	List<GetAllBrandsResponse> getAll();
+	void add(CreateBrandRequest createBrandRequest);
 
 }
--- rentACar/src/main/java/kodlama/io/rentACar/bussines/concretes/BrandManager.java
+++ rentACar/src/main/java/kodlama/io/rentACar/bussines/concretes/BrandManager.java
@@ -1,10 +1,13 @@
 package kodlama.io.rentACar.bussines.concretes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.bussines.abstracts.BrandService;
+import kodlama.io.rentACar.bussines.requests.CreateBrandRequest;
+import kodlama.io.rentACar.bussines.responses.GetAllBrandsResponse;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
@@ -17,9 +20,29 @@ public class BrandManager implements BrandService {
 	}
 
 	@Override
-	public List<Brand> getAll() {
+	public List<GetAllBrandsResponse> getAll() {
+		
+		List<Brand> brands = brandRepository.findAll();
+		List<GetAllBrandsResponse> brandsResponse = new ArrayList<GetAllBrandsResponse>();
+		for (Brand brand : brands) {
+			GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
+			responseItem.setId(brand.getId());
+			responseItem.setName(brand.getName());
+			
+			brandsResponse.add(responseItem);
+		}
 		//iş kural kodlarıı
-		return brandRepository.getAll();
+		return brandsResponse;
+	}
+
+	@Override
+	public void add(CreateBrandRequest createBrandRequest) {
+		
+		Brand brand = new Brand();
+		brand.setName(createBrandRequest.getName());
+		
+		this.brandRepository.save(brand);
+		
 	}
 
 }
