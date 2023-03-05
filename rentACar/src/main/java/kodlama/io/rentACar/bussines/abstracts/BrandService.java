76c78d5b8f602c0ece262d031f79b5828129f7be
alier <alier@LAPTOP-Q97OKIR9>
2023-03-06 00:38:24 +0300

    Commit 2

--- rentACar/src/main/java/kodlama/io/rentACar/bussines/abstracts/BrandService.java
+++ rentACar/src/main/java/kodlama/io/rentACar/bussines/abstracts/BrandService.java
@@ -3,10 +3,15 @@ package kodlama.io.rentACar.bussines.abstracts;
 import java.util.List;
 
 import kodlama.io.rentACar.bussines.requests.CreateBrandRequest;
+import kodlama.io.rentACar.bussines.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.bussines.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.bussines.responses.GetByIdBrandResponse;
 
 public interface BrandService {
 	List<GetAllBrandsResponse> getAll();
+	GetByIdBrandResponse getById(int id);
 	void add(CreateBrandRequest createBrandRequest);
+	void update(UpdateBrandRequest updateBrandRequest);
+	void delete(int id);
 
 }
